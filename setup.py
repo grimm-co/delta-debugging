1adfa2abe481dd747d32eb3a3182438666abf825
adam <adam@grimm-co.com>
2016-10-28 22:41:16 -0500

    Added a setup script and put the delta debugging script into its own package.

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name = "delta_debugging",
+      version = "1.10",
+      description = "Delta Debugging - debugging library to generate minimal crashing test cases",
+      author = ("Andreas Zeller, Martin Burger, " + 
+                "Holger Cleve, Karsten Lehmann, Stephan Neuhaus, " +
+                "and Tom Zimmermann.  Modified by Grimm."),
+      author_email = "github@grimm-co.com",
+      url = "https://github.com/grimm-co",
+      packages = ["delta_debugging"],
+      )
